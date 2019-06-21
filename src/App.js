511f7e48f282b85a870bb6a1c9d711f6a7e2008f
Jon T <jonathantayyw@gmail.com>
2019-06-22 07:21:16 +0800

    integrate singstat data

--- src/App.js
+++ src/App.js
@@ -1,76 +1,97 @@
 import React, { Component } from "react";
 import ReactEcharts from "echarts-for-react";
-import echarts from "echarts/lib/echarts";
-// import "echarts/lib/chart/bar";
-// import "echarts/lib/component/tooltip";
-import { dataMap } from "./Data";
+import { populationData } from "./Data";
 
 class App extends Component {
   getOption = () => {
+    let arr = [];
+    let dates = [];
+    let options = [];
+    Object.entries(populationData).forEach(entry => {
+      dates = [...dates, entry[0]];
+      entry[1].map(e => {
+        arr = [...arr, e.name];
+      });
+      let obj = {};
+      obj.title = {
+        text: `Singapore Population by District, ${entry[0]}`
+      };
+      obj.series = [
+        {
+          data: entry[1]
+        }
+      ];
+      options = [...options, obj];
+    });
+    let areas = [...new Set(arr)];
+
     return {
       baseOption: {
+        backgroundColor: "#fff",
+        color: ["#354EF6"],
         timeline: {
-          axisType: "category",
           autoPlay: true,
-          // bottom: 20,
-          // height: null,
-          // inverse: true,
-          // left: null,
-          // orient: 'vertical',
+          axisType: "category",
+          bottom: 20,
+          data: dates,
+          height: null,
+          inverse: true,
+          left: null,
+          orient: "vertical",
           playInterval: 1000,
-          // right: 0,
-          // top: 20,
-          // width: 55,
-          data: [
-            "2002-01-01",
-            "2003-01-01",
-            "2004-01-01",
-            {
-              value: "2005-01-01",
-              tooltip: {
-                formatter: "{b} GDP达到一个高度"
-              },
-              symbol: "diamond",
-              symbolSize: 16
-            },
-            "2006-01-01",
-            "2007-01-01",
-            "2008-01-01",
-            "2009-01-01",
-            "2010-01-01",
-            {
-              value: "2011-01-01",
-              tooltip: {
-                formatter: function(params) {
-                  return params.name + "GDP达到又一个高度";
-                }
-              },
-              symbol: "diamond",
-              symbolSize: 18
-            }
-          ],
+          right: 40,
+          top: 20,
+          width: 55,
           label: {
-            formatter: function(s) {
-              return new Date(s).getFullYear();
+            normal: {
+              textStyle: {
+                color: "#aaa"
+              },
+            },
+            emphasis: {
+              textStyle: {
+                color: "#333"
+              }
+            },
+          },
+          symbol: "none",
+          lineStyle: {
+            color: "#aaa"
+          },
+          checkpointStyle: {
+            color: "#333",
+            borderColor: "transparent",
+            borderWidth: 2
+          },
+          controlStyle: {
+            showNextBtn: false,
+            showPrevBtn: false,
+            normal: {
+              color: "#354EF6",
+              borderColor: "#354EF6"
+            },
+            emphasis: {
+              color: "#5d71f7",
+              borderColor: "#5d71f7"
             }
-          }
+          },
         },
         title: {
-          subtext: "数据来自国家统计局",
-          textAlign: 'center',
-          left: '50%',
-          top: '5%',
+          subtext: "Data from the Singapore Department of Statistics",
+          textAlign: "center",
+          left: "50%",
+          top: "0%"
         },
-        tooltip: {},
-        legend: {
-          x: "right",
-          data: ["第一产业", "第二产业", "第三产业", "GDP", "金融", "房地产"],
-          selected: {
-            GDP: false,
-            金融: false,
-            房地产: false
-          }
-        },
+        tooltip: { backgroundColor: "#333", borderWidth: 1, padding: 10 },
+        // legend: {
+        //   x: "right",
+        //   data: ["第一产业", "第二产业", "第三产业", "GDP", "金融", "房地产"],
+        //   selected: {
+        //     GDP: false,
+        //     金融: false,
+        //     房地产: false
+        //   }
+        // },
         calculable: true,
         grid: {
           top: 80,
@@ -90,248 +111,30 @@ class App extends Component {
         },
         xAxis: [
           {
-            type: "category",
-            axisLabel: { interval: 0 },
-            data: [
-              "北京",
-              "\n天津",
-              "河北",
-              "\n山西",
-              "内蒙古",
-              "\n辽宁",
-              "吉林",
-              "\n黑龙江",
-              "上海",
-              "\n江苏",
-              "浙江",
-              "\n安徽",
-              "福建",
-              "\n江西",
-              "山东",
-              "\n河南",
-              "湖北",
-              "\n湖南",
-              "广东",
-              "\n广西",
-              "海南",
-              "\n重庆",
-              "四川",
-              "\n贵州",
-              "云南",
-              "\n西藏",
-              "陕西",
-              "\n甘肃",
-              "青海",
-              "\n宁夏",
-              "新疆"
-            ],
-            splitLine: { show: false }
+            axisLabel: {
+              interval: 0,
+              rotate: 55,
+              textStyle: { baseline: "top", fontSize: 10 }
+            },
+            axisLine: { show: false },
+            axisTick: { show: false },
+            data: areas,
+            name: "District",
+            splitLine: { show: false },
+            type: "category"
           }
         ],
         yAxis: [
           {
-            type: "value",
-            name: "GDP（亿元）"
+            axisLine: { show: false },
+            axisTick: { show: false },
+            name: "Population",
+            type: "value"
           }
         ],
-        series: [
-          { name: "GDP", type: "bar" },
-          { name: "金融", type: "bar" },
-          { name: "房地产", type: "bar" },
-          { name: "第一产业", type: "bar" },
-          { name: "第二产业", type: "bar" },
-          { name: "第三产业", type: "bar" },
-          {
-            name: "GDP占比",
-            type: "pie",
-            center: ["75%", "35%"],
-            radius: "28%",
-            z: 100
-          }
-        ]
+        series: [{ name: "Total", type: "bar" }]
       },
-      options: [
-        {
-          title: { text: "2002全国宏观经济指标" },
-          series: [
-            { data: dataMap.dataGDP["2002"] },
-            { data: dataMap.dataFinancial["2002"] },
-            { data: dataMap.dataEstate["2002"] },
-            { data: dataMap.dataPI["2002"] },
-            { data: dataMap.dataSI["2002"] },
-            { data: dataMap.dataTI["2002"] },
-            {
-              data: [
-                { name: "第一产业", value: dataMap.dataPI["2002sum"] },
-                { name: "第二产业", value: dataMap.dataSI["2002sum"] },
-                { name: "第三产业", value: dataMap.dataTI["2002sum"] }
-              ]
-            }
-          ]
-        },
-        {
-          title: { text: "2003全国宏观经济指标" },
-          series: [
-            { data: dataMap.dataGDP["2003"] },
-            { data: dataMap.dataFinancial["2003"] },
-            { data: dataMap.dataEstate["2003"] },
-            { data: dataMap.dataPI["2003"] },
-            { data: dataMap.dataSI["2003"] },
-            { data: dataMap.dataTI["2003"] },
-            {
-              data: [
-                { name: "第一产业", value: dataMap.dataPI["2003sum"] },
-                { name: "第二产业", value: dataMap.dataSI["2003sum"] },
-                { name: "第三产业", value: dataMap.dataTI["2003sum"] }
-              ]
-            }
-          ]
-        },
-        {
-          title: { text: "2004全国宏观经济指标" },
-          series: [
-            { data: dataMap.dataGDP["2004"] },
-            { data: dataMap.dataFinancial["2004"] },
-            { data: dataMap.dataEstate["2004"] },
-            { data: dataMap.dataPI["2004"] },
-            { data: dataMap.dataSI["2004"] },
-            { data: dataMap.dataTI["2004"] },
-            {
-              data: [
-                { name: "第一产业", value: dataMap.dataPI["2004sum"] },
-                { name: "第二产业", value: dataMap.dataSI["2004sum"] },
-                { name: "第三产业", value: dataMap.dataTI["2004sum"] }
-              ]
-            }
-          ]
-        },
-        {
-          title: { text: "2005全国宏观经济指标" },
-          series: [
-            { data: dataMap.dataGDP["2005"] },
-            { data: dataMap.dataFinancial["2005"] },
-            { data: dataMap.dataEstate["2005"] },
-            { data: dataMap.dataPI["2005"] },
-            { data: dataMap.dataSI["2005"] },
-            { data: dataMap.dataTI["2005"] },
-            {
-              data: [
-                { name: "第一产业", value: dataMap.dataPI["2005sum"] },
-                { name: "第二产业", value: dataMap.dataSI["2005sum"] },
-                { name: "第三产业", value: dataMap.dataTI["2005sum"] }
-              ]
-            }
-          ]
-        },
-        {
-          title: { text: "2006全国宏观经济指标" },
-          series: [
-            { data: dataMap.dataGDP["2006"] },
-            { data: dataMap.dataFinancial["2006"] },
-            { data: dataMap.dataEstate["2006"] },
-            { data: dataMap.dataPI["2006"] },
-            { data: dataMap.dataSI["2006"] },
-            { data: dataMap.dataTI["2006"] },
-            {
-              data: [
-                { name: "第一产业", value: dataMap.dataPI["2006sum"] },
-                { name: "第二产业", value: dataMap.dataSI["2006sum"] },
-                { name: "第三产业", value: dataMap.dataTI["2006sum"] }
-              ]
-            }
-          ]
-        },
-        {
-          title: { text: "2007全国宏观经济指标" },
-          series: [
-            { data: dataMap.dataGDP["2007"] },
-            { data: dataMap.dataFinancial["2007"] },
-            { data: dataMap.dataEstate["2007"] },
-            { data: dataMap.dataPI["2007"] },
-            { data: dataMap.dataSI["2007"] },
-            { data: dataMap.dataTI["2007"] },
-            {
-              data: [
-                { name: "第一产业", value: dataMap.dataPI["2007sum"] },
-                { name: "第二产业", value: dataMap.dataSI["2007sum"] },
-                { name: "第三产业", value: dataMap.dataTI["2007sum"] }
-              ]
-            }
-          ]
-        },
-        {
-          title: { text: "2008全国宏观经济指标" },
-          series: [
-            { data: dataMap.dataGDP["2008"] },
-            { data: dataMap.dataFinancial["2008"] },
-            { data: dataMap.dataEstate["2008"] },
-            { data: dataMap.dataPI["2008"] },
-            { data: dataMap.dataSI["2008"] },
-            { data: dataMap.dataTI["2008"] },
-            {
-              data: [
-                { name: "第一产业", value: dataMap.dataPI["2008sum"] },
-                { name: "第二产业", value: dataMap.dataSI["2008sum"] },
-                { name: "第三产业", value: dataMap.dataTI["2008sum"] }
-              ]
-            }
-          ]
-        },
-        {
-          title: { text: "2009全国宏观经济指标" },
-          series: [
-            { data: dataMap.dataGDP["2009"] },
-            { data: dataMap.dataFinancial["2009"] },
-            { data: dataMap.dataEstate["2009"] },
-            { data: dataMap.dataPI["2009"] },
-            { data: dataMap.dataSI["2009"] },
-            { data: dataMap.dataTI["2009"] },
-            {
-              data: [
-                { name: "第一产业", value: dataMap.dataPI["2009sum"] },
-                { name: "第二产业", value: dataMap.dataSI["2009sum"] },
-                { name: "第三产业", value: dataMap.dataTI["2009sum"] }
-              ]
-            }
-          ]
-        },
-        {
-          title: { text: "2010全国宏观经济指标" },
-          series: [
-            { data: dataMap.dataGDP["2010"] },
-            { data: dataMap.dataFinancial["2010"] },
-            { data: dataMap.dataEstate["2010"] },
-            { data: dataMap.dataPI["2010"] },
-            { data: dataMap.dataSI["2010"] },
-            { data: dataMap.dataTI["2010"] },
-            {
-              data: [
-                { name: "第一产业", value: dataMap.dataPI["2010sum"] },
-                { name: "第二产业", value: dataMap.dataSI["2010sum"] },
-                { name: "第三产业", value: dataMap.dataTI["2010sum"] }
-              ]
-            }
-          ]
-        },
-        {
-          title: { text: "2011全国宏观经济指标" },
-          series: [
-            { data: dataMap.dataGDP["2011"] },
-            { data: dataMap.dataFinancial["2011"] },
-            { data: dataMap.dataEstate["2011"] },
-            { data: dataMap.dataPI["2011"] },
-            { data: dataMap.dataSI["2011"] },
-            { data: dataMap.dataTI["2011"] },
-            {
-              data: [
-                { name: "第一产业", value: dataMap.dataPI["2011sum"] },
-                { name: "第二产业", value: dataMap.dataSI["2011sum"] },
-                { name: "第三产业", value: dataMap.dataTI["2011sum"] }
-              ]
-            }
-          ]
-        }
-      ]
+      options: options
     };
   };
 
@@ -339,14 +142,10 @@ class App extends Component {
     return (
       <div className="App">
         <ReactEcharts
-          // echarts={echarts}
           option={this.getOption()}
           notMerge={true}
           lazyUpdate={true}
-          style={{height: '100vh', width: '100vw'}}
-          // theme={"theme_name"}
-          // onChartReady={this.onChartReadyCallback}
-          // onEvents={EventsDict}
+          style={{ height: "80vh", top: 50, width: "90vw" }}
           opts={{ renderer: "svg" }}
         />
       </div>
